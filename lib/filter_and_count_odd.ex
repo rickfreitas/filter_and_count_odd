b9e2b33fa05513df952190cbff0166ae9002b3f7
RickFreitas <henrique.manoel@hotmail.com.br>
2022-02-17 09:33:26 -0300

    initial commit

---
+++ lib/filter_and_count_odd.ex
@@ -0,0 +1,18 @@
+defmodule FilterAndCountOdd do
+  @moduledoc """
+  Documentation for `FilterAndCountOdd`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FilterAndCountOdd.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/filter_and_count_odd_test.exs
@@ -0,0 +1,8 @@
+defmodule FilterAndCountOddTest do
+  use ExUnit.Case
+  doctest FilterAndCountOdd
+
+  test "greets the world" do
+    assert FilterAndCountOdd.hello() == :world
+  end
+end
