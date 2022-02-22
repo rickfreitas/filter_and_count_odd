7d78c34390cc59c737b9a5850bea13e69ca75e25
RickFreitas <henrique.manoel@hotmail.com.br>
2022-02-22 10:33:59 -0300

    filter and count

--- lib/filter_and_count_odd.ex
+++ lib/filter_and_count_odd.ex
@@ -1,18 +1,22 @@
 defmodule FilterAndCountOdd do
-  @moduledoc """
-  Documentation for `FilterAndCountOdd`.
-  """
+  require Integer
 
-  @doc """
-  Hello world.
+  def call(list), do: filter_and_count_odd(list)
 
-  ## Examples
-
-      iex> FilterAndCountOdd.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def filter_and_count_odd(list) do
+    Enum.reduce(list, 0, fn elem, acc -> acc + check_and_format(elem) end)
   end
+
+  def check_and_format(n) do
+    n_as_number = turn_to_number(Integer.parse(n))
+
+    if(Integer.is_odd(n_as_number)) do
+      1
+    else
+      0
+    end
+  end
+
+  def turn_to_number(:error), do: 0
+  def turn_to_number({n, _}), do: n
 end
--- test/filter_and_count_odd_test.exs
+++ test/filter_and_count_odd_test.exs
@@ -1,8 +1,17 @@
-defmodule FilterAndCountOddTest do
+defmodule ListLengthTest do
   use ExUnit.Case
-  doctest FilterAndCountOdd
 
-  test "greets the world" do
-    assert FilterAndCountOdd.hello() == :world
+  describe "call/1" do
+    test "count odd elements" do
+      list1 = ["1", "3", "6", "43", "banana", "6", "abc"]
+
+      assert FilterAndCountOdd.call(list1) == 3
+    end
+
+    test "passing an empty list" do
+      list = []
+
+      assert FilterAndCountOdd.call(list) == 0
+    end
   end
 end
